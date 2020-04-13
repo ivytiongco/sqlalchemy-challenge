d8974f6cda95b728e2fccb9d672ffd8e665ceb17
Ivy Tiongco <ivydoreen@gmail.com>
2020-04-13 16:56:10 -0700

    add py file

---
+++ climate_app.py
@@ -0,0 +1,64 @@
+# Flask setup
+app = Flask(__name__)
+
+# Flask routes
+@app.route("/")
+def welcome():
+    """List all available api routes."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/passengers<br/>"
+        f"/api/v1.0/tobs<br/>"
+        f"/api/v1.0/<start><br/>"
+        f"/api/v1.0/<start>/<end>"
+    )
+
+
+@app.route("/api/v1.0/precipitation")
+def names():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of all passenger names"""
+    # Query all passengers
+    results = session.query(Passenger.name).all()
+
+    session.close()
+
+    # Convert list of tuples into normal list
+    all_names = list(np.ravel(results))
+
+    return jsonify(all_names)
+
+
+# use     return jsonify(justice_league_members)
+# for stations portion below
+@app.route("/api/v1.0/stations")
+def passengers():
+    # Create our session (link) from Python to the DB
+    session = Session(engine)
+
+    """Return a list of passenger data including the name, age, and sex of each passenger"""
+    # Query all passengers
+    results = session.query(Passenger.name, Passenger.age, Passenger.sex).all()
+
+    session.close()
+
+    # Create a dictionary from the row data and append to a list of all_passengers
+    all_passengers = []
+    for name, age, sex in results:
+        passenger_dict = {}
+        passenger_dict["name"] = name
+        passenger_dict["age"] = age
+        passenger_dict["sex"] = sex
+        all_passengers.append(passenger_dict)
+
+    return jsonify(all_passengers)
+
+@app.route("/api/v1.0/tobs")
+@app.route("/api/v1.0/<start>")
+@app.route("/api/v1.0/<start>/<end>")
+
+if __name__ == '__main__':
+    app.run(debug=True)
